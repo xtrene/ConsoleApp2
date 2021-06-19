29f943db79b6ffa3e3aa4553d83301ebd22b9614
Carlos <Carlos@xtrene.com>
2021-06-19 23:03:36 +0200

    Tarea2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,19 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hola Mundo !!!");
+            int num1 = 5;
+            int nuero2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+            Console.WriteLine(suma);
+            string valor1 = Console.ReadLine();
+            Console.WriteLine("El número introducido es: " + valor1);
+
+            Console.ReadKey();
+
         }
     }
 }
